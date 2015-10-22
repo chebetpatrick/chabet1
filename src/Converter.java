3e16a15b33c603b4aaafc677b392e70efd3038c2
M-UNIT <mugabevedward@gmail.com>
2015-10-22 19:42:46 +0300

    my work sir kevin

---
+++ src/Converter.java
@@ -0,0 +1,27 @@
+
+import javax.swing.JFrame;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Edward T
+ */
+public class Converter {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        converter.Converter frame = new converter.Converter();
+			frame.setSize(400,300);
+			frame.setTitle("CONVERTER");
+			frame.setVisible(true);
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			frame.setLocationRelativeTo(null);
+    }
+}
